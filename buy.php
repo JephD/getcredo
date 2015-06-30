1558b68c41ca14829501d7e1138853e13f8bbf2f
JephD <jephdavid@gmail.com>
2015-06-30 19:26:23 +0300

    Added a buy airtime form

---
+++ buy.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Buy Airtime</title>
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Online Buy Airtime</legend>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="phonenumber">Phone Number</label>
+  <div class="controls">
+    <input id="phonenumber" name="phonenumber" type="text" placeholder="0712345678" class="input-xlarge" required="">
+
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="amount">Amount</label>
+  <div class="controls">
+    <input id="amount" name="amount" type="text" placeholder="100" class="input-xlarge" required="">
+
+  </div>
+</div>
+
+<!-- Button -->
+<div class="control-group">
+  <label class="control-label" for="submit"></label>
+  <div class="controls">
+    <button id="submit" name="submit" class="btn btn-success">Buy</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+  </body>
+</html>
